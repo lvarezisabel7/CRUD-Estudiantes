afb996e540857bc68577b5f0dabe736118e3aed6
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-05 16:54:22 +0100

    Add Estudiante en clase del paq basico

--- src/main/java/com/example/CrudEstudiantesApplication.java
+++ src/main/java/com/example/CrudEstudiantesApplication.java
@@ -1,13 +1,35 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.entities.Estudiante;
+import com.example.entities.Genero;
+import com.example.services.EstudianteService;
+
+import lombok.RequiredArgsConstructor;
+
 @SpringBootApplication
-public class CrudEstudiantesApplication {
+@RequiredArgsConstructor
+public class CrudEstudiantesApplication implements CommandLineRunner{
+
+	private final EstudianteService estudianteService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudEstudiantesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Estudiante est1 = Estudiante.builder()
+				.nombre("Isabel")
+				.primerApellido("Alvarez")
+				.segundoApellido("Sanchez")
+				.genero(Genero.MUJER)
+				.build();
+
+		estudianteService.persistirEstudiante(est1);
 	}
 
 }
