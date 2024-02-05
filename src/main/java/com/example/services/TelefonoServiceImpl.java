688b648ba2137868a15d1dfa8961c01f08c3c814
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-05 18:22:18 +0100

    Add TelefonoService y algunas cosas en Impl

---
+++ src/main/java/com/example/services/TelefonoServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.services;
+
+import java.util.List;
+
+import com.example.dao.EstudianteDao;
+import com.example.dao.TelefonoDao;
+import com.example.entities.Telefono;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TelefonoServiceImpl implements TelefonoService {
+     
+    private final TelefonoDao telefonoDao;
+    private final EstudianteDao estudianteDao;
+
+    @Override
+    public List<Telefono> telefonos(int idEstudiante) {
+        return telefonoDao.findByEstudiante(estudianteDao.findById(idEstudiante).get());
+    }
+
+    @Override
+    public void eliminarTelefonos(int idEstudiante) {
+        telefonoDao.deleteByEstudiante(estudianteDao.findById(idEstudiante).get());
+    }
+
+    @Override
+    public void persistirTelefono(int idEstudiante, Telefono telefono) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'persistirTelefono'");
+    }
+
+}
