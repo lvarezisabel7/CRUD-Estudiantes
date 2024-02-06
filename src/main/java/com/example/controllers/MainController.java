a9381dd022a027a0124dd634bdcbd19106e4c12a
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-06 17:27:15 +0100

    Add procesar telefonos

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,14 +1,22 @@
 package com.example.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entities.Estudiante;
+import com.example.entities.Telefono;
 import com.example.services.CursoService;
 import com.example.services.EstudianteService;
 
@@ -56,6 +64,34 @@ public class MainController {
 
         return "views/frmAltaModificacionEstudiante";
     }
+
+    @PostMapping("/persistir")
+    @Transactional
+    public String persistirEstudiante(@ModelAttribute(name = "estudiante") Estudiante estudiante,
+            @RequestParam(name = "numerosTel", required = false) String telefonosRecibidos,
+            @RequestParam(name = "direccionesCorreo", required = false) String correosRecibidos) {
+        
+        // Procesar los telefonos
+        if(telefonosRecibidos != null) {
+            String[] arrayTelefonos = telefonosRecibidos.split(";");
+            List<String> numerosTelefonos = Arrays.asList(arrayTelefonos);
+
+            List<Telefono> telefonos = new ArrayList<>();
+
+            numerosTelefonos.stream()
+                .forEach(numeroTelefono -> {
+                    telefonos.add(Telefono.builder()
+                        .telefono(numeroTelefono)
+                        .estudiante(estudiante)
+                        .build());
+                });
+
+            estudiante.setTelefonos(telefonos);
+
+            }
+                estudianteService.persistirEstudiante(estudiante);
+                return "redirect:/all";
+            }
 
 
 }
