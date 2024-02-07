ca504f23b320af0c8c506947c84968ded23377a9
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-07 17:04:19 +0100

    Add metodo en CursoDao

--- src/main/java/com/example/dao/CursoDao.java
+++ src/main/java/com/example/dao/CursoDao.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.entities.Curso;
+import com.example.entities.Horario;
+
+import java.util.List;
+
 
 @Repository
 public interface CursoDao extends JpaRepository<Curso, Integer>{
-   
+
+    public List<Curso> findByHorario(Horario horario);
 
 }
