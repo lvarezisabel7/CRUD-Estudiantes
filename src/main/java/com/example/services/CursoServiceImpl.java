2e819708122338af0455f756bf055f69156bb438
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-07 17:20:48 +0100

    Add metodo en cursoServiceImpl

--- src/main/java/com/example/services/CursoServiceImpl.java
+++ src/main/java/com/example/services/CursoServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.example.dao.CursoDao;
+import com.example.dao.EstudianteDao;
 import com.example.entities.Curso;
+import com.example.entities.Estudiante;
+import com.example.entities.Horario;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,4 +34,11 @@ public class CursoServiceImpl implements CursoService{
         cursoDao.save(curso);
     }
 
+    @Override
+    public List<Estudiante> dameEstudiantesPorHorario(Horario horario) {
+        return cursoDao.findByHorario(horario).stream()
+            .flatMap(curso -> curso.getEstudiante().stream())
+            .collect(Collectors.toList());
+
+    }
 }
